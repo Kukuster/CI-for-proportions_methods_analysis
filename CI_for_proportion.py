1b1d169fd0b4ede8dbde1ef998a2bddd76d3e982
Kukuster <KukusterMOP@gmail.com>
2021-03-30 18:34:37 +0300

    + plan to add CI for the diff between p1 & p2. Improvements on the simulation

--- CI_for_proportion.py
+++ CI_for_proportion.py
@@ -59,20 +59,26 @@ CI_method = Callable[
 ]
 
 
-# x - succeeded trials
-# n - total trials
-# conflevel - confidence level (0 < float < 1). Defaults to 0.95 if its unset and *z* is unset
-# z - z score. If unset, calculated form the given *conflevel*
 def wald_interval(x: int, n: int, conflevel: Union[float, None] = 0.95, z: Union[float, None] = None):
     # LaTeX: $$(w^-, w^+) = \hat{p}\,\pm\,z\sqrt{\frac{\hat{p}(1-\hat{p})}{n}}$$
+    """Calculates confidence interval for proportions using Wald Interval method
+
+    `x` - succeeded trials
+
+    `n` - total trials
+
+    `conflevel` - confidence level (0 < float < 1). Defaults to 0.95 if its unset and *z* is unset
+
+    `z` - z score. If unset, calculated form the given *conflevel*
+    """
     if x > n:
         raise ValueError(f"Number of succeeded trials (x) has to be no more than number of total trials (n). x = {x} and n = {n} were passed")
-    p = x/n
     if z is None:
         if conflevel is None:
             conflevel = 0.95
         z = zScore_normal(conflevel)
-    
+
+    p = float(x)/n
     sd = math.sqrt((p*(1-p))/n)
     z_sd = z*sd
     ci = (
@@ -82,20 +88,26 @@ def wald_interval(x: int, n: int, conflevel: Union[float, None] = 0.95, z: Union
     return ci
 
 
-# x - succeeded trials
-# n - total trials
-# conflevel - confidence level (0 < float < 1). Defaults to 0.95 if its unset and *z* is unset
-# z - z score. If unset, calculated form the given *conflevel*
 def wilson_score_interval(x: int, n: int, conflevel: Union[float, None] = 0.95, z: Union[float, None] = None):
     # LaTeX: $$(w^-, w^+) = \frac{p + z^2/2n \pm z\sqrt{p(1-p)/n + z^2/4n^2}}{1+z^2/n}$$
+    """Calculates confidence interval for proportions using Wilson Score Interval method
+
+    `x` - succeeded trials
+
+    `n` - total trials
+
+    `conflevel` - confidence level (0 < float < 1). Defaults to 0.95 if its unset and *z* is unset
+
+    `z` - z score. If unset, calculated form the given *conflevel*
+    """
     if x > n:
         raise ValueError(f"Number of succeeded trials (x) has to be no more than number of total trials (n). x = {x} and n = {n} were passed")
-    p = x/n
     if z is None:
         if conflevel is None:
             conflevel = 0.95
         z = zScore_normal(conflevel)
     
+    p = float(x)/n
     denom = 1 + ((z**2) / n)
     mean = p + ((z**2)/(2*n))
     diff = z * math.sqrt(p*(1-p)/n + (z**2)/(4*n**2))
@@ -106,10 +118,6 @@ def wilson_score_interval(x: int, n: int, conflevel: Union[float, None] = 0.95, 
     return ci
 
 
-# x - succeeded trials
-# n - total trials
-# conflevel - confidence level (0 < float < 1). Defaults to 0.95 if its unset and *z* is unset
-# z - z score. If unset, calculated form the given *conflevel*
 def wilson_score_interval_continuity_corrected(x: int, n: int, conflevel: Union[float, None] = 0.95, z: Union[float, None] = None):
     # LaTeX:
     # $$w_{cc}^- = \frac{2np + z^2 - (z\sqrt{z^2 - 1/n + 4np(1-p) + (4p-2)} + 1)}{2(n+z^2)}$$
@@ -118,14 +126,24 @@ def wilson_score_interval_continuity_corrected(x: int, n: int, conflevel: Union[
     # $$e = 2np + z^2;\,\,\, f = z^2 - 1/n + 4np(1-p);\,\,\, g = (4p - 2);\,\,\, h = 2(n+z^2)$$
     # $$w_{cc}^- = \frac{e - (z\sqrt{f+g} + 1)}{h}$$
     # $$w_{cc}^+ = \frac{e + (z\sqrt{f-g} + 1)}{h}$$
+    """Calculates confidence interval for proportions using Wilson Score Interval method with correction for continuity
+
+    `x` - succeeded trials
+
+    `n` - total trials
+
+    `conflevel` - confidence level (0 < float < 1). Defaults to 0.95 if its unset and *z* is unset
+
+    `z` - z score. If unset, calculated form the given *conflevel*
+    """
     if x > n:
         raise ValueError(f"Number of succeeded trials (x) has to be no more than number of total trials (n). x = {x} and n = {n} were passed")
-    p = float(x)/n
     if z is None:
         if conflevel is None:
             conflevel = 0.95
         z = zScore_normal(conflevel)
     
+    p = float(x)/n
     e = 2*n*p + z**2
     f = z**2 - 1/n + 4*n*p*(1-p)
     g = (4*p - 2)
@@ -137,14 +155,32 @@ def wilson_score_interval_continuity_corrected(x: int, n: int, conflevel: Union[
     return ci
 
 
-# x - succeeded trials
-# n - total trials
-# conflevel - confidence level (0 < float < 1). Defaults to 0.95 if its unset and *z* is unset
-# z - z score. If unset, calculated form the given *conflevel*
 def wilson_score_interval_continuity_semicorrected(x: int, n: int, conflevel: Union[float, None] = 0.95, z: Union[float, None] = None):
+    """Calculates confidence interval for proportions using two Wilson Score Interval methods
+    (arithmetic mean of ordinary and continuity-corrected methods)
+
+    `x` - succeeded trials
+
+    `n` - total trials
+
+    `conflevel` - confidence level (0 < float < 1). Defaults to 0.95 if its unset and *z* is unset
+
+    `z` - z score. If unset, calculated form the given *conflevel*
+    """
+    if x > n:
+        raise ValueError(
+            f"Number of succeeded trials (x) has to be no more than number of total trials (n). x = {x} and n = {n} were passed")
+    if z is None:
+        if conflevel is None:
+            conflevel = 0.95
+        z = zScore_normal(conflevel)
+    
     uncorrected = wilson_score_interval(x, n, conflevel, z)
     corrected   = wilson_score_interval_continuity_corrected(x, n, conflevel, z)
-    ci = ((corrected[0]+uncorrected[0])/2, (corrected[1]+uncorrected[1])/2)
+    ci = (
+        (corrected[0]+uncorrected[0])/2,
+        (corrected[1]+uncorrected[1])/2
+    )
     return ci
 
 
@@ -211,7 +247,7 @@ def calculate_and_plot_coverage(numSamples: int, numTrials: int,
 
 numSamples = 50000
 # numTrials = 40000
-step = Decimal('0.000001')
+step = Decimal('0.0000001')
 probs = list(frange(Decimal('0.000001'), Decimal('0.000199'), step))
 conflevel = 0.95
 
--- README.md
+++ README.md
@@ -1,5 +1,50 @@
-# CI-for-proportions_methods_analysis
-Simulating efficacy of various methods for calculating confidence interval for proportions
+# CI for proportions methods analysis
+Calculating efficacy with simulation of various methods for calculating confidence interval for proportions
 
 sources:
 https://towardsdatascience.com/five-confidence-intervals-for-proportions-that-you-should-know-about-7ff5484c024f
+
+
+## PLAN (2021.03.29)
+
+### Plottting
+
+To figure out the best method for calculating _**CI for the difference between two proportions**_, I have to make one more simulation and plot it as a 2d-histogram:
+
+https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.hist2d.html
+
+The idea is that I would have a `dark_background` style, with a gradient colors. e.g. for a 95%CI:
+ - 95% coverage - white color,
+ - 50% coverage - red color,
+ - 100% - green or blue
+
+
+### Simulating methods
+
+Start with this document:
+
+**[https://ncss-wpengine.netdna-ssl.com/wp-content/themes/ncss/pdf/Procedures/PASS/Confidence_Intervals_for_the_Difference_Between_Two_Proportions.pdf](https://ncss-wpengine.netdna-ssl.com/wp-content/themes/ncss/pdf/Procedures/PASS/Confidence_Intervals_for_the_Difference_Between_Two_Proportions.pdf) **
+
+Figure out how exaclty to implement the methods and evaluate their efficacy.
+
+E.g. as I understood  the **Farrington and Manning’s Score** method, the main idea is to calculate 2 values for delta0, that is the confidence interval for the delta (which equals to p1_hat - p2_hat)
+
+For this, we calculate everything and get zFMD = F(delta0), where F is some sick humongous function over the unknown variable - delta0. It involves polynomials of delta0 up to 4th power, cosines and arccosines of that, and a square root over all that. Then the the delta0 is calculated as a solution for zFMD = F(delta0) for zFMD equals +-z-score for the given alpha. It's very likely that it is impossible to solve this analytically (hence such a recipe). Therefore, I have to figure out a way to implement numerical methods for solving this in general form.
+
+By the way, I think "MLE" there means "Maximum Likelihood Estimate", but I'm not sure.
+
+## Links
+**1. Equivalence and Noninferiority Testing (as I understand, are fancy terms for 2-sided and 1-sided p tests for the difference between two proportions)**
+ - **[https://ncss-wpengine.netdna-ssl.com/wp-content/themes/ncss/pdf/Procedures/PASS/Non-Inferiority_Tests_for_the_Difference_Between_Two_Proportions.pdf](https://ncss-wpengine.netdna-ssl.com/wp-content/themes/ncss/pdf/Procedures/PASS/Non-Inferiority_Tests_for_the_Difference_Between_Two_Proportions.pdf) **
+ - [https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3019319/](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3019319/)
+ - [https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2701110/](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2701110/)
+ - [https://pubmed.ncbi.nlm.nih.gov/9595617/](https://pubmed.ncbi.nlm.nih.gov/9595617/)
+**2. Biostatistics course (Dr. Nicolas Padilla Raygoza, et al.)**
+ - [https://docs.google.com/presentation/d/1t1DowyVDDRFYGHDlJgmYMRN4JCrvFl3q/edit#slide=id.p1](https://docs.google.com/presentation/d/1t1DowyVDDRFYGHDlJgmYMRN4JCrvFl3q/edit#slide=id.p1) 
+ - [https://www.google.com/search?q=Dr.+Sc.+Nicolas+Padilla+Raygoza+Biostatistics+course+Part+10&oq=Dr.+Sc.+Nicolas+Padilla+Raygoza+Biostatistics+course+Part+10&aqs=chrome..69i57.3448j0j7&sourceid=chrome&ie=UTF-8](https://www.google.com/search?q=Dr.+Sc.+Nicolas+Padilla+Raygoza+Biostatistics+course+Part+10&oq=Dr.+Sc.+Nicolas+Padilla+Raygoza+Biostatistics+course+Part+10&aqs=chrome..69i57.3448j0j7&sourceid=chrome&ie=UTF-8) 
+ - [https://slideplayer.com/slide/9837395/](https://slideplayer.com/slide/9837395/)
+**3. Using z-test instead of a binomial test:**
+ - When can use [https://stats.stackexchange.com/questions/424446/when-can-we-use-a-z-test-instead-of-a-binomial-test](https://stats.stackexchange.com/questions/424446/when-can-we-use-a-z-test-instead-of-a-binomial-test) 
+ - How to use [https://cogsci.ucsd.edu/~dgroppe/STATZ/binomial_ztest.pdf](https://cogsci.ucsd.edu/~dgroppe/STATZ/binomial_ztest.pdf) 
+**4. A concern of the vaccine causing Bell's palsy**
+ - [https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7874945/](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7874945/) or [https://www.sciencedirect.com/science/article/pii/S266635462100020X?via%3Dihub](https://www.sciencedirect.com/science/article/pii/S266635462100020X?via%3Dihub) 
